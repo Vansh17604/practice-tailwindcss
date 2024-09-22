ae39c7a6a5472187a29d1f16d56f30ca2683c90f
Vansh17604 <vansh456patel@gmail.com>
2024-09-22 13:24:14 +0530

    my first tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Componets/Footer";
+import Header from "./Componets/Header";
+import Body from "./Componets/Body";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-16 lg:py-10 ">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
